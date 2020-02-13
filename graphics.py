b08cd4373a9bdbb7bddc3f8c146db33653dd918c
Theodore Wu <twu00@stuy.edu>
2020-02-13 15:10:11 -0500

    ugh

--- graphics.py
+++ graphics.py
@@ -1,35 +1,23 @@
-class Pixel:
-    DEFAULT = [0,0,0]
-    def __init__(self, r = DEFAULT[0], g = DEFAULT[0], b = DEFAULT[0]):
-        self.color = [r,g,b]
-    def setColor(self, r = -1, g = -1, b = -1, color = []):
-        if len(color) == 3:
-            self.color = color[:]
-            return
-        for h in range(3):
-            if [r,g,b][h] != -1:
-                self.color[h] = [r,g,b][h]
-    def getColor(self):
-        return "{} {} {} ".format(self.color[0], self.color[1], self.color[2])
 class Screen:
+    DEFAULT = [0,0,0]
     def __init__(self, h, w):
-        self.pixels = [[Pixel() for i in range(w)] for i in range(h)]
+        self.pixels = [[Screen.DEFAULT[:] for i in range(w)] for i in range(h)]
         self.height = h
         self.width = w
     def clear(self):
-        for h in self.pixels:
-            for w in h:
-                w.color = Pixel.DEFAULT[:]
+        for h in self.height:
+            for w in self.width:
+                self.pixels[h][w] = Screen.DEFAULT[:]
     def toFile(self,file):
         enter = "P6\n{} {}\n255\n".format(self.height, self.width)
         with open(file+".ppm", "wb") as f:
             f.write(enter.encode())
             for h in range(self.height):
                 for w in range(self.width):
-                    c = self.pixels[h][w].color
+                    c = self.pixels[h][w]
                     f.write(bytes(c))
     def toFileAscii(self,file):
-        enter = "P3\n{} {}\n255\n".format(self.height, self.width)
+        enter = "P6\n{} {}\n255\n".format(self.height, self.width)
         for i in self.pixels:
             row = ""
             for p in i:
@@ -46,16 +34,16 @@ class Screen:
             enter += row + "\n"
         print(enter)
     def allPix(self, funct): #funct is a function that takes in a pixel and modifies it
-        for r in self.pixels:
-            for c in r:
-                funct(c)
+        for h in self.height:
+            for w in self.width:
+                funct(self.pixels[h][w])
     def allPixCoord(self, funct):
         for h in range(self.height):
             for w in range(self.width):
                 funct(w,h,self.pixels[w][h])
-    def plot(self,w, h, color, r = 0, g = 0, b = 0):
+    def plot(self,w, h,color):
         #print(h,w)
-        self.pixels[h][w].setColor(r,g,b,color)
+        self.pixels[h][w] = color[:]
     def _Q1(self,x1,y1,x2,y2,color):
         a = y2 - y1
         b = -(x2-x1)
@@ -157,5 +145,8 @@ c[2] = 0;
 c[1] = 255;
 image.line(0, int(YRES/2), XRES-1, int(YRES/2), c);
 image.line(int(XRES/2), 0, int(XRES/2), YRES-1, c);
+
+#save_ppm(image, "pic.ppm")
+image.plot(250,250,[255,255,255])
 image.toFile("pic")
 print("pic.ppm")
