a4380e011b5a451f6f2d90a6d1d3edc675fc65ad
Theodore Wu <twu00@stuy.edu>
2020-02-27 15:20:00 -0500

    fixed bug

--- graphics.py
+++ graphics.py
@@ -114,7 +114,7 @@ class Screen:
         else:
             self._Q4(x1,y1,x2,y2,color)
 class Matrix:
-    def __init__(self, points = 4):
+    def __init__(self, points = 0):
         c = [0,0,0,1]
         self.data = [c[:] for i in range(points)]
         self.id = False
@@ -130,26 +130,34 @@ class Matrix:
         self.data = [[0,0,0,1],[0,0,1,0],[0,1,0,0],[1,0,0,0]][::-1]
         self.id = True
     def trns(self, a = 0, b = 0, c = 0):
-        self.indent()
+        self.ident()
         self.data[0][3] = a
         self.data[1][3] = b
         self.data[2][3] = c
     def scale(self, a = 1, b = 1, c = 1):
-        self.indent()
+        self.ident()
         self.data[0][0] = a
         self.data[1][1] = b
         self.data[2][2] = c
     def rotate(self, axis, deg):
-        self.indent()
+        self.ident()
         rad = radians(deg)
         m = self.data
         if axis.equals("x"):
-            None
+            m[1][1] = cos(rad)
+            m[1][2] = -sin(rad)
+            m[2][1] = sin(rad)
+            m[2][2] = cos(rad)
         elif axis.equals("y"):
-            None
+            m[2][2] = cos(rad)
+            m[2][0] = -sin(rad)
+            m[0][2] = sin(rad)
+            m[0][0] = cos(rad)
         elif axis.equals("z"):
             m[0][0] = cos(rad)
             m[0][1] = -sin(rad)
+            m[1][0] = sin(rad)
+            m[1][1] = cos(rad)
         else:
             print ("wrong input given to rotate: invalid axis")
             raise ValueError
@@ -159,7 +167,30 @@ class Matrix:
             ori = self.data[p]
             for i in range(4):
                 f = m.data[i]
-                new[i] = f[0]*ori[i] + f[1]*ori[i] + f[2]*ori[i] + f[3]*ori[i]
+                new[i] = f[0]*ori[0] + f[1]*ori[1] + f[2]*ori[2] + f[3]*ori[3]
             self.data[p] = new
-        
+    def addLine(self,x1,y1,z1,x2,y2,z2):
+        self.data.append([x1,y1,z1,1])
+        self.data.append([x2,y2,z2,1])
+    def toScreen(self, screen):
+        l = 0
+        while l < len(self.data):
+            screen.line(self.data[l][0],self.data[l][1], self.data[l+1][0], self.data[l+1][1],[255,255,255])
+            l+=2
+pic = Screen(500,500)
+lines = Matrix(0)
+lines.addLine(0,0,0,250,0,0)
+lines.addLine(0,0,0,0,250,0)
+lines.addLine(0,250,0,250,250,0)
+lines.addLine(250,0,0,250,250,0)
+move = Matrix()
+move.trns(125,125,0)
+lines.print()
+move.print()
+
+lines.mult(move)
+lines.print()
+#lines.addLine(0,0,0,0,0,0)
+lines.toScreen(pic)
+pic.toFile("pic")
 
